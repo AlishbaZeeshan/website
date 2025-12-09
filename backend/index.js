9029674b70fefd00d16dbc3757d00b92c2720c07
Alishba Zeeshan <alishbazeshan78@gmail.com>
2025-12-09 10:43:24 +0500

    host

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://your-frontend-domain.vercel.app"
+    "https://learnify-rho-two.vercel.app"
+
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
